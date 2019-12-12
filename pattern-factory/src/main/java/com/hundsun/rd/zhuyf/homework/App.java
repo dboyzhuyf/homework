dd84effc297a2d8139538e5f7844105fbc51009f
dboyzhuyf <dboyzhuyf@foxmail.com>
2019-12-12 16:28:40 +0800

    pattern factory init commit

---
+++ pattern-factory/src/main/java/com/hundsun/rd/zhuyf/homework/App.java
@@ -0,0 +1,13 @@
+package com.hundsun.rd.zhuyf.homework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
