37ec940f7536da28dc59969c3931c0574c2a2cc4
dboyzhuyf <dobyzhuyf@foxmail.com>
2020-03-04 00:39:32 +0800

    代理模式作业提交。

---
+++ pattern/gupaoedu-vip-pattern-proxy/src/main/java/com/gupaoedu/vip/pattern/proxy/dynamicproxy/gpproxy/client/BuetifulGirl.java
@@ -0,0 +1,32 @@
+package com.gupaoedu.vip.pattern.proxy.dynamicproxy.gpproxy.client;
+
+public class BuetifulGirl {
+
+    private String name;
+    private String address;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
